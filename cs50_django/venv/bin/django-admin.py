93de56aaed50e3a275f766f417804241fc4be2ab
Andrews <andyboat75@gmail.com>
2020-05-12 23:23:33 +0000

    Getting started

---
+++ cs50_django/venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/andrews/Desktop/edx/cs50/web_programming_python_javascript/cs50_django/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
